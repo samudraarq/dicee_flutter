cf22a04c8393e5060c7dedab9ccf6616e69323c2
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-29 10:20:36 +0700

    add randomise function

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void randomiseDiceNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,9 +40,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
+                randomiseDiceNumber();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -43,9 +49,7 @@ class _DicePageState extends State<DicePage> {
             child: TextButton(
               child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+                randomiseDiceNumber();
               },
             ),
           ),
